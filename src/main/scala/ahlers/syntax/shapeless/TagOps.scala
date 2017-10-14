85c5a816acb2f70dd515eeb0f5dc52cab7110c96
Michael Ahlers <michael@ahlers.consulting>
2017-10-14 12:35:54 -0600

    Add sugars for Shapeless

---
+++ src/main/scala/ahlers/syntax/shapeless/TagOps.scala
@@ -0,0 +1,19 @@
+package ahlers.syntax.shapeless
+
+import shapeless.tag
+import shapeless.tag._
+
+import scala.language.implicitConversions
+
+/**
+ * Make it easier to tag values.
+ * @see [[https://github.com/milessabin/shapeless/issues/557 milessabin/shapeless#557]]
+ * @author <a href="michael@ahlers.consulting">Michael Ahlers</a>
+ */
+final case class TagOps[T](self: T) extends AnyVal {
+	def tagged[U]: T @@ U = tag[U].apply[T](self)
+}
+
+trait ToTagOps {
+	implicit def implyTagOps[T](t: T): TagOps[T] = TagOps(t)
+}
---
+++ src/main/scala/ahlers/syntax/shapeless/package.scala
@@ -0,0 +1,9 @@
+package ahlers.syntax
+
+/**
+ * Syntactic sugars.
+ * @author <a href="michael@ahlers.consulting">Michael Ahlers</a>
+ */
+package object shapeless {
+	object tags extends ToTagOps
+}
