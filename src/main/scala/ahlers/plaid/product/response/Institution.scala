c9df9548afa85815a319801f656e674839b6da72
Michael Ahlers <michael@ahlers.consulting>
2017-10-14 10:30:31 -0600

    Define an Institution model

---
+++ src/main/scala/ahlers/plaid/product/response/Institution.scala
@@ -0,0 +1,53 @@
+package ahlers.plaid.product.response
+
+import shapeless.tag._
+import ahlers.plaid.product.common.{ Product => CommonProduct }
+
+/**
+ * @author <a href="michael@ahlers.consulting">Michael Ahlers</a>
+ */
+object Institution {
+
+	object Tags {
+		trait Id
+		trait Name
+		trait MultiFactorAuthentication
+
+		type MFA = MultiFactorAuthentication
+	}
+
+	type Id = String @@ Tags.Id
+	type Name = String @@ Tags.Name
+	type MultiFactorAuthentication = String @@ Tags.MultiFactorAuthentication
+	type MFA = MultiFactorAuthentication
+
+	object Credential {
+
+		object Tags {
+			trait Label
+			trait Name
+			trait Type
+		}
+
+		type Label = String @@ Tags.Label
+		type Name = String @@ Tags.Name
+		type Type = String @@ Tags.Type
+
+	}
+
+	case class Credential(
+		label: Credential.Label,
+		name: Credential.Name,
+		`type`: Credential.Type
+	)
+
+}
+
+case class Institution(
+	id: Institution.Id,
+	name: Institution.Name,
+	credentials: List[Institution.Credential],
+	hasMFA: Boolean,
+	MFAs: List[Institution.MFA],
+	products: List[CommonProduct]
+)
