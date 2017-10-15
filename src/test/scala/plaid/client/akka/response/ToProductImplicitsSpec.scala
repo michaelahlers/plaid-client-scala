bbab060394d0e0bae4586738c21322426fb8ae82
Michael Ahlers <michael@ahlers.consulting>
2017-10-14 22:00:35 -0600

    Test institution conversion

---
+++ src/test/scala/plaid/client/akka/response/ToProductImplicitsSpec.scala
@@ -0,0 +1,27 @@
+package plaid.client.akka.response
+
+import com.plaid.{ client => reference }
+import org.scalatest._
+import org.scalatest.prop.GeneratorDrivenPropertyChecks.{ forAll => forAllProp }
+import plaid.client.akka.response.ImplicitConversions._
+
+import scala.language.postfixOps
+
+/**
+ * @author <a href="michael@ahlers.consulting">Michael Ahlers</a>
+ */
+class ToProductImplicitsSpec extends WordSpec
+	with Matchers with Inspectors
+	with ReferenceComparisons {
+
+	"Implicit conversion" must {
+
+		s"return ${classOf[Institution]} from ${classOf[reference.response.Institution]}" in {
+			forAllProp(reference.response.Generators.Institutions.gen) { sample =>
+				verifyInstitution(sample, sample)
+			}
+		}
+
+	}
+
+}
