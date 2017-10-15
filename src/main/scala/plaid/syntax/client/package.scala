4e8fea812cb978755fbfba4f7edb5bc995504a0b
Michael Ahlers <michael@ahlers.consulting>
2017-10-15 16:03:25 -0600

    Add forgotten commit

---
+++ src/main/scala/plaid/syntax/client/package.scala
@@ -0,0 +1,8 @@
+package plaid.syntax
+
+/**
+ * @author <a href="michael@ahlers.consulting">Michael Ahlers</a>
+ */
+package object client {
+	object credentialProviders extends ToCredentialProviderOps
+}
