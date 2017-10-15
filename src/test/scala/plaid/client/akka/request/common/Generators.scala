2be1e54e065ece68662fe501fa40413a34eed257
Michael Ahlers <michael@ahlers.consulting>
2017-10-15 11:14:41 -0600

    Add generators for this project's types

---
+++ src/test/scala/plaid/client/akka/request/common/Generators.scala
@@ -0,0 +1,14 @@
+package plaid.client.akka.request.common
+
+import org.scalacheck._
+
+/**
+ * @author <a href="michael@ahlers.consulting">Michael Ahlers</a>
+ */
+object Generators {
+
+	object Products {
+		val gen = Gen.oneOf(Product.values)
+	}
+
+}
