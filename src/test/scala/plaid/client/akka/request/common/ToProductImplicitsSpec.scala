391bdeb13db64264b0f1136ceeac12823b7de982
Michael Ahlers <michael@ahlers.consulting>
2017-10-14 19:02:46 -0600

    Clean up test

--- src/test/scala/plaid/client/akka/request/common/ToProductImplicitsSpec.scala
+++ src/test/scala/plaid/client/akka/request/common/ToProductImplicitsSpec.scala
@@ -1,32 +1,36 @@
 package plaid.client.akka.request.common
 
-import com.plaid.client.request.{ common => plaid }
+import com.plaid.client.request.{ common => reference }
 import org.scalatest._
-import ImplicitConversions._
+import plaid.client.akka.request.common.ImplicitConversions._
 
 /**
  * @author <a href="michael@ahlers.consulting">Michael Ahlers</a>
  */
-class ToProductImplicitsSpec extends WordSpec with Matchers {
+class ToProductImplicitsSpec extends WordSpec
+	with Matchers with Inspectors {
 
-	"Implicit conversion" that {
-		s"takes ${classOf[plaid.Product]}" must {
+	"Implicit conversion" must {
+
+		s"return ${classOf[Product]} from ${classOf[reference.Product]}" in {
+			import Product._
+			import reference.Product._
+
 			val scenarios =
-				(plaid.Product.AUTH, Product.Authentication) ::
-					(plaid.Product.BALANCE, Product.Balance) ::
-					(plaid.Product.CREDIT_DETAILS, Product.CreditDetails) ::
-					(plaid.Product.IDENTITY, Product.Identity) ::
-					(plaid.Product.INCOME, Product.Income) ::
-					(plaid.Product.TRANSACTIONS, Product.Transactions) ::
+				(AUTH, Authentication) ::
+					(BALANCE, Balance) ::
+					(CREDIT_DETAILS, CreditDetails) ::
+					(IDENTITY, Identity) ::
+					(INCOME, Income) ::
+					(TRANSACTIONS, Transactions) ::
 					Nil
 
-			scenarios foreach {
-				case (p, expected) =>
-					s"convert $p to $expected" in {
-						implyProduct(p) should be(expected)
-					}
+			forAll(scenarios) {
+				case (reference, expected) =>
+					(reference: Product) should be(expected)
 			}
 		}
+
 	}
 
 }
