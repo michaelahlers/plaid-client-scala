3af04fe5b003d37478203f21bcd5f4b4ce41dfe6
Michael Ahlers <michael@ahlers.consulting>
2017-10-23 17:50:15 -0600

    Property-based testing here is overkill

--- src/test/scala/plaid/client/PromiseCallbackSpec.scala
+++ src/test/scala/plaid/client/PromiseCallbackSpec.scala
@@ -2,53 +2,17 @@ package plaid.client
 
 import com.plaid.client.PlaidClient
 import com.plaid.client.response.ErrorResponse
+import org.mockito.ArgumentMatchers._
 import org.mockito.Mockito._
-import org.scalacheck.{ Arbitrary, Gen }
 import org.scalatest._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.mockito.MockitoSugar
-import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import plaid.client.PromiseCallback._
-import plaid.client.PromiseCallbackSpec.Generators
 import retrofit2.{ Call, Response }
-import org.mockito.ArgumentMatchers._
 /**
  * @author <a href="michael@ahlers.consulting">Michael Ahlers</a>
  */
-object PromiseCallbackSpec {
-
-	trait Generators { self: MockitoSugar =>
-
-		object Calls {
-			def gen[T]: Gen[Call[T]] =
-				for {
-					cancelled <- Arbitrary.arbBool.arbitrary
-				} yield {
-					val call = mock[Call[T]]
-					when(call.isCanceled()).thenReturn(cancelled)
-					call
-				}
-		}
-
-		object Responses {
-			def gen[T](implicit a: Arbitrary[T]): Gen[Response[T]] =
-				for {
-					successful <- Arbitrary.arbBool.arbitrary
-					body <- Arbitrary.arbitrary[T]
-				} yield {
-					val response = mock[Response[T]]
-					when(response.isSuccessful()).thenReturn(successful)
-					when(response.body()).thenReturn(body)
-					response
-				}
-		}
-
-	}
-
-}
-
 class PromiseCallbackSpec extends WordSpec with Matchers
-	with Generators with GeneratorDrivenPropertyChecks
 	with ScalaFutures
 	with MockitoSugar {
 
@@ -86,47 +50,44 @@ class PromiseCallbackSpec extends WordSpec with Matchers
 
 		}
 
-		val scenarios =
-			for {
-				call <- Calls.gen[Body]
-				response <- Responses.gen[Body]
-			} yield (call, response)
-
 		"propagate cancellations" in {
-			forAll(scenarios) {
-				case (call, response) =>
-					whenever(call.isCanceled) {
-						val callback = PromiseCallback[Body](mock[PlaidClient])
-						callback.onResponse(call, response)
-						callback.promise.future.futureValue should be(Cancellation)
-					}
-			}
+			val call = mock[Call[Body]]
+			when(call.isCanceled()).thenReturn(true)
+
+			val response = mock[Response[Body]]
+			verifyNoMoreInteractions(response)
+
+			val callback = PromiseCallback[Body](mock[PlaidClient])
+			callback.onResponse(call, response)
+			callback.promise.future.futureValue should be(Cancellation)
 		}
 
 		"propagate success" in {
-			forAll(scenarios) {
-				case (call, response) =>
-					whenever(!call.isCanceled && response.isSuccessful) {
-						val callback = PromiseCallback[Body](mock[PlaidClient])
-						callback.onResponse(call, response)
-						callback.promise.future.futureValue should be(Success(response))
-					}
-			}
+			val call = mock[Call[Body]]
+			when(call.isCanceled()).thenReturn(false)
+
+			val response = mock[Response[Body]]
+			when(response.isSuccessful()).thenReturn(true)
+
+			val callback = PromiseCallback[Body](mock[PlaidClient])
+			callback.onResponse(call, response)
+			callback.promise.future.futureValue should be(Success(response))
 		}
 
 		"propagate errors" in {
-			forAll(scenarios) {
-				case (call, response) =>
-					whenever(!call.isCanceled && !response.isSuccessful) {
-						val client = mock[PlaidClient]
-						val error = mock[ErrorResponse]
-						when(client.parseError(response)).thenReturn(error)
+			val call = mock[Call[Body]]
+			when(call.isCanceled()).thenReturn(false)
 
-						val callback = PromiseCallback[Body](client)
-						callback.onResponse(call, response)
-						callback.promise.future.futureValue should be(Failure(error))
-					}
-			}
+			val response = mock[Response[Body]]
+			when(response.isSuccessful()).thenReturn(false)
+
+			val client = mock[PlaidClient]
+			val error = mock[ErrorResponse]
+			when(client.parseError(response)).thenReturn(error)
+
+			val callback = PromiseCallback[Body](client)
+			callback.onResponse(call, response)
+			callback.promise.future.futureValue should be(Failure(error))
 		}
 
 	}
