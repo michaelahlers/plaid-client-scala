c59d7cfa1f43dca8dc87fdc8b5b4f8c10fd5adad
Michael Ahlers <michael@ahlers.consulting>
2018-01-20 18:35:07 -0500

    Add credential provider generators

--- src/test/scala/plaid/client/CredentialProviderSpec.scala
+++ src/test/scala/plaid/client/CredentialProviderSpec.scala
@@ -1,34 +1,18 @@
 package plaid.client
 
 import org.scalacheck.Gen
+import org.scalacheck.Gen._
+import org.scalacheck.ScalacheckShapeless._
+import plaid.client.CredentialSpec.Generators._
 
 /**
  * @author <a href="michael@ahlers.consulting">Michael Ahlers</a>
  */
 object CredentialProviderSpec {
-
-	object Generators extends Generators
-	trait Generators {
-
-		import CredentialSpec.Generators._
-		import Gen._
-
+	object Generators {
 		object CredentialProviders {
 			private case class DirectCredentialProvider(credential: Option[Credential]) extends CredentialProvider
-
-			val gen: Gen[CredentialProvider] =
-				for {
-					credentialO <- option(Credentials.gen)
-				} yield DirectCredentialProvider(credentialO)
-
-			def samples: Stream[CredentialProvider] =
-				Stream.continually(gen).flatMap(_.sample)
-
-			def sample(filter: CredentialProvider => Boolean): CredentialProvider =
-				samples.filter(filter).head
-
+			val gen: Gen[CredentialProvider] = option(Credentials.gen).map(DirectCredentialProvider)
 		}
-
 	}
-
 }
