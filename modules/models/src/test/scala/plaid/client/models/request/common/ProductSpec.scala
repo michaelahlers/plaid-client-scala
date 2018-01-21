f0c57dc624cd65d26328d9eb335ccc1dcae667ad
Michael Ahlers <michael@ahlers.consulting>
2018-01-20 19:22:04 -0500

    Use ScalaCheck Shapeless for generator

---
+++ modules/models/src/test/scala/plaid/client/models/request/common/ProductSpec.scala
@@ -0,0 +1,16 @@
+package plaid.client.models.request.common
+
+import org.scalacheck.Arbitrary._
+import org.scalacheck.ScalacheckShapeless._
+import org.scalacheck._
+
+/**
+ * @author <a href="michael@ahlers.consulting">Michael Ahlers</a>
+ */
+object ProductSpec {
+	object Generators {
+		object Products {
+			val gen: Gen[Product] = arbitrary[Product]
+		}
+	}
+}
