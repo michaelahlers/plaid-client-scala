bbf4bb2edb7197dc9d7e37f65a70e15cbe0a3c5e
Michael Ahlers <michael@ahlers.consulting>
2018-01-28 12:44:29 -0700

    Make construction easier

--- src/main/scala/plaid/client/Credential.scala
+++ src/main/scala/plaid/client/Credential.scala
@@ -1,16 +1,21 @@
 package plaid.client
 
+import plaid.client.support.syntax.shapeless.tags._
 import shapeless.tag._
 
 /**
  * @author <a href="michael@ahlers.consulting">Michael Ahlers</a>
  */
+case class Credential(client: Credential.Client, secret: Credential.Key, public: Option[Credential.Key])
 object Credential {
 	object Tags {
 		trait Key
 	}
 
 	type Key = String @@ Tags.Key
+	object Key {
+		def apply(v: String): Key = v.tagged[Tags.Key]
+	}
 
 	object Client {
 		object Tags {
@@ -18,13 +23,13 @@ object Credential {
 		}
 
 		type Id = String @@ Tags.Id
+		object Id {
+			def apply(v: String): Id = v.tagged[Tags.Id]
+		}
+
 	}
 
 	case class Client(id: Client.Id)
-}
 
-case class Credential(
-	client: Credential.Client,
-	secret: Credential.Key,
-	public: Option[Credential.Key]
-)
+	def apply(client: Credential.Client, secret: Credential.Key): Credential = Credential(client, secret, None)
+}
