82f36e035114e1e48d0eb322ead50cef97ac3400
Michael Ahlers <michael@ahlers.consulting>
2018-02-04 16:44:50 -0700

    Implement credential providers for system properties and environment variables

--- src/main/scala/plaid/client/CredentialProvider.scala
+++ src/main/scala/plaid/client/CredentialProvider.scala
@@ -1,5 +1,14 @@
 package plaid.client
 
+import plaid.client.Credential.Client._
+import plaid.client.Credential._
+import plaid.client.commons.shapeless.TaggerCompanion
+import shapeless.tag._
+import plaid.client.CredentialProvider.Named._
+
+import scala.collection.Map
+import scala.sys.SystemProperties
+
 /**
  * @author <a href="michael@ahlers.consulting">Michael Ahlers</a>
  */
@@ -8,7 +17,26 @@ trait CredentialProvider {
 }
 
 object CredentialProvider {
+
 	private case class Direct(credential: Option[Credential]) extends CredentialProvider
-	def apply(c: Option[Credential]): CredentialProvider = Direct(c)
-	def apply(c: Credential): CredentialProvider = CredentialProvider(Some(c))
+	def direct(c: Option[Credential]): CredentialProvider = Direct(c)
+	def direct(c: Credential): CredentialProvider = direct(Some(c))
+
+	object Named {
+		trait Name
+		object Name extends TaggerCompanion[String, Name]
+		case class Names(clientId: String @@ Name, secretKey: String @@ Name, publicKey: String @@ Name)
+	}
+
+	case class Named[A <: Map[String, String]](names: Names, values: A) extends CredentialProvider {
+		override def credential =
+			for {
+				clientId <- values.get(names.clientId)
+				secretKey <- values.get(names.secretKey)
+			} yield Credential(Client(Id(clientId)), Key(secretKey), values.get(names.publicKey).map(Key(_)))
+	}
+
+	val systemProperties: CredentialProvider = Named(Names(Name("plaid.client.id"), Name("plaid.secret.key"), Name("plaid.public.key")), sys.props)
+	val environmentVariables: CredentialProvider = Named(Names(Name("PLAID_CLIENT_ID"), Name("PLAID_SECRET_KEY"), Name("PLAID_PUBLIC_KEY")), sys.env)
+
 }
--- src/test/scala/plaid/client/CredentialProviderSpec.scala
+++ src/test/scala/plaid/client/CredentialProviderSpec.scala
@@ -1,9 +1,18 @@
 package plaid.client
 
-import org.scalacheck.Gen
+import org.scalacheck._
 import org.scalacheck.Gen._
+import org.scalacheck.Arbitrary._
 import org.scalacheck.ScalacheckShapeless._
+import org.scalatest._
+import org.scalatest.Matchers._
 import plaid.client.CredentialSpec.Generators._
+import org.scalatest.prop.GeneratorDrivenPropertyChecks._
+import plaid.client.CredentialProviderSpec.Generators
+import plaid.client.support.syntax.shapeless.tags._
+import shapeless.tag._
+import CredentialSpec.Generators._
+import org.scalamock.scalatest.MockFactory
 
 /**
  * @author <a href="michael@ahlers.consulting">Michael Ahlers</a>
@@ -15,4 +24,76 @@ object CredentialProviderSpec {
 			val gen: Gen[CredentialProvider] = option(Credentials.gen).map(DirectCredentialProvider)
 		}
 	}
+}
+
+class CredentialProviderSpec extends WordSpec with MockFactory {
+
+	implicit val arbCredential = Arbitrary(Credentials.gen)
+
+	"Direct" must {
+		import CredentialProvider.direct
+		"provide given credential" in {
+			forAll { credential: Option[Credential] =>
+				direct(credential).credential should be(credential)
+			}
+		}
+	}
+
+	"Named" must {
+		import CredentialProvider.systemProperties
+		import CredentialProvider.environmentVariables
+		import CredentialProvider.Named
+		import Named._
+
+		"specify correct names and values" when {
+			"providing system properties" in {
+				systemProperties should have(
+					'values(sys.props),
+					'names(Names(
+						Name("plaid.client.id"),
+						Name("plaid.secret.key"),
+						Name("plaid.public.key")
+					))
+				)
+			}
+			"providing environment variables" in {
+				environmentVariables should have(
+					'values(sys.env),
+					'names(Names(
+						Name("PLAID_CLIENT_ID"),
+						Name("PLAID_SECRET_KEY"),
+						Name("PLAID_PUBLIC_KEY")
+					))
+				)
+			}
+		}
+
+		"provide equivalent credential" in {
+			implicit val arbName = Arbitrary {
+				for {
+					clientId <- arbitrary[String]
+					secretKey <- arbitrary[String]
+					publicKey <- arbitrary[String]
+					if 3 == Set(clientId, secretKey, publicKey).size
+				} yield Names(Name(clientId), Name(secretKey), Name(publicKey))
+			}
+
+			forAll { (names: Names, credential: Credential) =>
+				val provider = Named(names, {
+					val values =
+						Some(names.clientId -> credential.client.id) ::
+							Some(names.secretKey -> credential.secret) ::
+							credential.public.map(names.publicKey -> _) ::
+							Nil
+
+					values.flatten.toMap[String, String]
+				})
+
+				provider.credential should contain(credential)
+			}
+
+		}
+
+	}
+
 }
