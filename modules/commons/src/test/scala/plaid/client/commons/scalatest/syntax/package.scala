90e9287ba7535292879a919fffdc51160120ff43
Michael Ahlers <michael@ahlers.consulting>
2018-03-09 20:36:49 -0500

    Publish a syntax package

---
+++ modules/commons/src/test/scala/plaid/client/commons/scalatest/syntax/package.scala
@@ -0,0 +1,9 @@
+package plaid.client.commons.scalatest
+
+/**
+ * Syntactic support for ScalaTest.
+ * @author <a href="michael@ahlers.consulting">Michael Ahlers</a>
+ */
+package object syntax {
+	object matchers extends MatcherInstances
+}
