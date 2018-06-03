68a2910e4039012d560b6e43debcd4c8f46edea2
Michael Ahlers <michael@ahlers.consulting>
2018-06-02 23:10:41 -0400

    Require boundaries

--- modules/models/src/main/scala/plaid/client/models/request/InstitutionsGetRequest.scala
+++ modules/models/src/main/scala/plaid/client/models/request/InstitutionsGetRequest.scala
@@ -3,4 +3,8 @@ package plaid.client.models.request
 /**
  * @author <a href="michael@ahlers.consulting">Michael Ahlers</a>
  */
-case class InstitutionsGetRequest(offset: Int, length: Int)
+case class InstitutionsGetRequest(offset: Int, length: Int) {
+	require(0 <= offset)
+	require(0 < length)
+	require(500 >= length)
+}
