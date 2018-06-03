a094f6aed42aea3978ab61982e12aee80db60042
Michael Ahlers <michael@ahlers.consulting>
2018-06-02 23:04:16 -0400

    Add shorthand notation for the reference type

---
+++ modules/clients/reference/src/main/scala/plaid/client/models/request/package.scala
@@ -0,0 +1,8 @@
+package plaid.client.models
+
+/**
+ * @author <a href="mailto:michael@ahlers.consulting>Michael Ahlers</a>
+ */
+package object request {
+	type RefInstitutionsGetRequest = com.plaid.client.request.InstitutionsGetRequest
+}
